f9ff9a354a1a2accb4a104d7c71ac9c5f1ef2041
coldJune <785691557@qq.com>
2018-03-15 16:36:33 +0800

    计算矩阵

---
+++ ipython/experiment.py
@@ -0,0 +1,16 @@
+import numpy as np
+from numpy.linalg import eigvals
+
+
+def run_experiment(niter=100):
+    k = 100
+    results = []
+    for _ in range(niter):
+        mat = np.random.randn(k, k)
+        max_eigenvalue = np.abs(eigvals(mat)).max()
+        results.append(max_eigenvalue)
+    return results
+
+some_results = run_experiment()
+print('Largest one we saw:%s' % np.max(some_results))
+
