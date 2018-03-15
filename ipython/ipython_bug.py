0244d9ca3711877fc3aa45d44c3e2792e147e63b
coldJune <785691557@qq.com>
2018-03-15 11:38:26 +0800

    添加IPython测试文件

---
+++ ipython/ipython_bug.py
@@ -0,0 +1,7 @@
+def f(x, y, z):
+    return (x+y) /z
+
+a = 1
+b = 2
+c = 0
+result = f(a, b, c)
---
+++ ipython/ipython_script_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+
+def f(x, y, z):
+    return (x+y) /z
+
+a = 1
+b = 2
+c = 3
+result = f(a, b, c)
